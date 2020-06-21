28e645a99b632759a2228f0a1d38aa9625dfc2f8
KhalCamargo <67078716+KhalCamargo@users.noreply.github.com>
2020-06-21 15:04:12 -0300

    Create read_xlsx.py

---
+++ read_xlsx.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+xls = pd.read_excel('Dados Tempestade Cirrus.xlsx', sheet_name='Planilha2', usecols = "X,AB")
+xls = xls.to_numpy()
+
+erroPitch = xls[:,0]
+cmdPitch = xls[:,1]
+
+print(erroPitch)
+print(cmdPitch)
