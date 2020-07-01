68ddb69fd085b38d363e84fb51c3f01721b94054
hessrafael <46797053+hessrafael@users.noreply.github.com>
2020-07-01 17:10:07 -0300

    Add files via upload
    Codigo XPlane. py (esse que deve modificar), .pkl com os dados salvos em pandas, Clustering_pitch apenas para visualização depois (regras tao erradas)

--- Clustering_Pitch.py
+++ Clustering_Pitch.py
@@ -4,6 +4,139 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import skfuzzy.control as ctrl
 
+def getData():
+        # Gathering all data
+    data = pd.read_excel('C:\\Workspace\\XPlane\\Dados USAR SO PRA PITCH 3.xlsx')
+    data = data.drop(data.index[0:159]) #Removing n-first rows
+
+    data2 = pd.read_excel('C:\\Workspace\\XPlane\\Dados USAR SO PRA PITCH 2.xlsx')
+    data2 = data.drop(data.index[0:284]) #Removing n-first rows
+
+    data3 = pd.read_excel('C:\\Workspace\\XPlane\\Dados USAR SO PRA PITCH.xlsx')
+    data3 = data.drop(data.index[0:1003]) #Removing n-first rows
+
+    # Appending data
+    data = data.append(data2)
+    data = data.append(data3)
+
+    data = data.loc[(abs(data['   pitch,__deg '])) <= 15] #Selecting only pitch within range
+    data = data.loc[:,'ErroPitch':]
+
+    ##### New Code
+    datas = data.loc[:,['ErroPitch','dErroP','CmdPitch']]
+
+    #datas.loc[:,'ErroPitch':'dErroP'] = (datas.loc[:,'ErroPitch':'dErroP'] - datas.loc[:,'ErroPitch':'dErroP'].min())/(datas.loc[:,'ErroPitch':'dErroP'].max()-datas.loc[:,'ErroPitch':'dErroP'].min())
+    return datas
+
+def normalizeDatas(datas):
+    datas = (datas - datas.min())/(datas.max()-datas.min())
+    return datas
+def normalizeNewData(data,datas):    
+    toClassDF = {'ErroPitch':[data[0][0]],
+                 'dErroP':[data[0][1]]}
+    toClass = pd.DataFrame(toClassDF,columns=['ErroPitch','dErroP'])
+    toClass = (toClass -  datas.min())/(datas.max()-datas.min())
+    if toClass > 1:
+        toClass = 1
+    if toClass < 0:
+        toClass = 0
+    return toClass.to_numpy()
+def generateFuzzySys(datas):
+    datasnp = datas.to_numpy()
+    n_clusters = 7
+    cntrError, uError, u0Error, dError, jmError, pError, fpcError = fuzz.cluster.cmeans(datasnp.T, c=n_clusters, m=2, error=0.005, maxiter=1000, init=None)
+
+    cntrStick = []
+    for i in range (0,n_clusters):
+        cntrStick.append(cntrError[i,2])
+    cntrStick.sort()
+
+    ElevStick = ctrl.Consequent(np.arange(-1, 1.001, 0.001), 'Elevation Stick')
+    """ Saída Stick
+    SL - Subir Large
+    SM - Subir Medium
+    SS - Subir Small
+    M - Manter
+    DS - Descer Small
+    DM - Descer Medium
+    DL - Descer Large
+    """
+    ElevStick['DL'] = fuzz.trapmf(ElevStick.universe,[-2,-1,cntrStick[0],cntrStick[1]])
+    ElevStick['DM'] = fuzz.trimf(ElevStick.universe,[cntrStick[0],cntrStick[1],cntrStick[2]])
+    ElevStick['DS'] = fuzz.trimf(ElevStick.universe,[cntrStick[1],cntrStick[2],cntrStick[3]])
+    ElevStick['M'] = fuzz.trimf(ElevStick.universe,[cntrStick[2],cntrStick[3],cntrStick[4]])
+    ElevStick['SS'] = fuzz.trimf(ElevStick.universe,[cntrStick[3],cntrStick[4],cntrStick[5]])
+    ElevStick['SM'] = fuzz.trimf(ElevStick.universe,[cntrStick[4],cntrStick[5],cntrStick[6]])
+    ElevStick['SL'] = fuzz.trapmf(ElevStick.universe,[cntrStick[5],cntrStick[6],1,2])
+
+ 
+
+    ## New Rules
+    cntrErrorP = []
+    for i in range (0,n_clusters):
+        cntrErrorP.append(cntrError[i,0])
+    cntrErrorP.sort()
+
+    Error = ctrl.Antecedent(np.arange(0,1.001,0.001),'Error')
+    """ Entrada Cluster
+    NL-negative large
+    NM-negative medium
+    NS-negative small
+    AZ-approximately zero
+    PS-positive small
+    PM-positive medium
+    PL-positive large
+    """
+    Error['NL'] = fuzz.trapmf(Error.universe,[-2,-1,cntrErrorP[0],cntrErrorP[1]])
+    Error['NM'] = fuzz.trimf(Error.universe,[cntrErrorP[0],cntrErrorP[1],cntrErrorP[2]])
+    Error['NS'] = fuzz.trimf(Error.universe,[cntrErrorP[1],cntrErrorP[2],cntrErrorP[3]])
+    Error['Z'] = fuzz.trimf(Error.universe,[cntrErrorP[2],cntrErrorP[3],cntrErrorP[4]])
+    Error['PS'] = fuzz.trimf(Error.universe,[cntrErrorP[3],cntrErrorP[4],cntrErrorP[5]])
+    Error['PM'] = fuzz.trimf(Error.universe,[cntrErrorP[4],cntrErrorP[5],cntrErrorP[6]])
+    Error['PL'] = fuzz.trapmf(Error.universe,[cntrErrorP[5],cntrErrorP[6],1,2])
+
+    cntrDErrorP = []
+    for i in range (0,n_clusters):
+        cntrDErrorP.append(cntrError[i,1])
+    cntrDErrorP.sort()
+
+    dError = ctrl.Antecedent(np.arange(0,1.001,0.001),'Error Derivative')
+
+    dError['NL'] = fuzz.trapmf(dError.universe,[-2,-1,cntrDErrorP[0],cntrDErrorP[1]])
+    dError['NM'] = fuzz.trimf(dError.universe,[cntrDErrorP[0],cntrDErrorP[1],cntrDErrorP[2]])
+    dError['NS'] = fuzz.trimf(dError.universe,[cntrDErrorP[1],cntrDErrorP[2],cntrDErrorP[3]])
+    dError['Z'] = fuzz.trimf(dError.universe,[cntrDErrorP[2],cntrDErrorP[3],cntrDErrorP[4]])
+    dError['PS'] = fuzz.trimf(dError.universe,[cntrDErrorP[3],cntrDErrorP[4],cntrDErrorP[5]])
+    dError['PM'] = fuzz.trimf(dError.universe,[cntrDErrorP[4],cntrDErrorP[5],cntrDErrorP[6]])
+    dError['PL'] = fuzz.trapmf(dError.universe,[cntrDErrorP[5],cntrDErrorP[6],1,2])
+
+    allRules = list()
+
+    # Regras dos extremos
+    #R1 = ctrl.Rule(Error['NL'],ElevStick['SL'])
+    #allRules.append(R1)
+    #R2 = ctrl.Rule(Error['PL'],ElevStick['DL'])
+    #allRules.append(R2)
+    R3 = ctrl.Rule(((Error['NM'] | Error['NS']) & (dError['NL'] | dError['NM'])) | (Error['Z'] & dError['NL']) | (Error['NM'] & dError['NS'] | Error['NL']),ElevStick['SL'])
+    allRules.append(R3)
+    R4 = ctrl.Rule(((Error['PM'] | Error['PS']) & (dError['PL'] | dError['PM'])) | (Error['Z'] & dError['PL']) | (Error['PM'] & dError['PS'] | Error['PL']),ElevStick['DL'])
+    allRules.append(R4)
+    R5 = ctrl.Rule((Error['PS'] & dError['NL']) | (Error['Z'] & dError['NM']) | (Error['NS'] & dError['NS']) | (Error['NM'] & dError['Z']),ElevStick['SM'])
+    allRules.append(R5)
+    R6 = ctrl.Rule((Error['PM'] & dError['NL']) | (Error['PS'] & dError['NM']) | (Error['Z'] & dError['NS']) | (Error['NS'] & dError['Z']) | (Error['NM'] & dError['PS']),ElevStick['SS'])
+    allRules.append(R6)
+    R7 = ctrl.Rule((Error['PM'] & dError['NM']) | (Error['PS'] & dError['NS']) | (Error['Z'] & dError['Z']) | (Error['NS'] & dError['PS']) | (Error['NM'] & dError['PM']),ElevStick['M'])
+    allRules.append(R7)
+    R8 = ctrl.Rule((Error['PM'] & dError['NS']) | (Error['PS'] & dError['Z']) | (Error['Z'] & dError['PS']) | (Error['NS'] & dError['PM']) | (Error['NM'] & dError['PL']),ElevStick['DS'])
+    allRules.append(R8)
+    R9 = ctrl.Rule((Error['PM'] & dError['Z']) | (Error['PS'] & dError['PS']) | (Error['Z'] & dError['PM']) | (Error['NS'] & dError['PL']),ElevStick['DM'])
+    allRules.append(R9)
+
+    sys = ctrl.ControlSystem(allRules)
+    sim = ctrl.ControlSystemSimulation(sys)
+    return sim
+
+
 # Gathering all data
 data = pd.read_excel('C:\\Workspace\\XPlane\\Dados USAR SO PRA PITCH 3.xlsx')
 data = data.drop(data.index[0:159]) #Removing n-first rows
